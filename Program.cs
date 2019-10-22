b43d73476b02884ec8a820aa38072fa21558f1ca
Alistair Chapman <alistair@agchapman.com>
2019-10-23 00:55:37 +1000

    Template console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xplat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Brisbane!");
+        }
+    }
+}
